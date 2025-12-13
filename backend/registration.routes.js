cc48a5022d4abe796b875a1b7ea2f79179dadc56
satheshsa1003-debug <satheshsa1003@gmail.com>
2025-12-13 23:55:34 +0530

    Delete backend directory

--- backend/registration.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as registrationController from "../controller/registration.controller.js";
-
-const router=express.Router();
-router.post('/register', registrationController.registerUser);
-router.post('/login', registrationController.loginUser);
-
-export default router;
-
